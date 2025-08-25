9ac2adfcf5d767a789f896b8d0498b68cce52688
John Bowman <bowman@ualberta.ca>
2025-08-24 18:16:03 -0700

    Revert "Defer dependent buffer creation until after call to render()."
    This reverts commit 1d68c3d03cebfd870f9ae7d7f5670a510a98b244.

--- vkrender.cc
+++ vkrender.cc
@@ -2674,8 +2674,8 @@ void AsyVkRender::createBuffers()
     frameObj.uboMappedMemory = make_unique<vma::cxx::MemoryMapperLock>(frameObj.uboBf);
   }
 
-//  createMaterialAndLightBuffers();
-//  createDependentBuffers();
+  createMaterialAndLightBuffers();
+  createDependentBuffers();
 }
 
 
@@ -2777,10 +2777,8 @@ void AsyVkRender::createImmediateRenderTargets()
 
 void AsyVkRender::createDependentBuffers()
 {
-//  redraw=true;
-//  render(); // Determine whether the scene is opaque.
-//  redisplay=true;
-  cout << "createDependent" << endl;
+  render(); // Determine whether the scene is opaque.
+  redisplay=true;
 
   pixels=Opaque ? 1 : (backbufferExtent.width+1)*(backbufferExtent.height+1);
 
@@ -4449,6 +4447,7 @@ void AsyVkRender::clearBuffers()
 
 void AsyVkRender::render()
 {
+
 #ifdef HAVE_PTHREAD
   static bool first=true;
   if(vkthread && first) {
@@ -4462,7 +4461,6 @@ void AsyVkRender::render()
 #endif
 
   if(redraw) {
-    cout << "render" << endl;
     clearData();
 
     if(remesh)
